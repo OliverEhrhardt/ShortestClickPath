6220aeb76155c76a8b026fe9937cbd6125bbbcb4
OliverEhrhardt <oliver.ehrhardt0@gmail.com>
2015-07-26 00:34:13 -0600

    added start and end nodes
    start is green, end is red

--- main.js
+++ main.js
@@ -1,88 +1,91 @@
 var nodes = []; //global array of all nodes on screen
 var edges = []; //global array of all edges on screen
+var isDrag = false; //check drag
+var startNode, endNode; //start and end node of pathfinding algorithm
 
-$(window).ready(windowClick);
+//Dijkstra's Algorithm - highlights shortest path
+function shortestPath(start, end){
+	
+}
 
-function windowClick(e){
-	$('#grid').attr("height", $(document).height());
-	$('#grid').attr("width", $(document).width());
-	$('body').click(function(e){
-		//display node data
-		var nodeContainer = $("<div>").addClass('node');
-		$("body").append(nodeContainer);
-		nodeContainer.css({
-			top: e.clientY - nodeContainer.height()/2,
-			left: e.clientX - nodeContainer.width()/2
+//drag function for start and end nodes
+function makeDraggable(el){
+	el.on('mousedown', function(){
+		isDrag = true;
+		$('body').on('mousemove', function(moveEvent){
+			el.css({
+				top: moveEvent.clientY - el.height()/2,
+				left: moveEvent.clientX - el.width()/2
+			});
 		});
-		//create node data
-		function createNode(){
-			var node = {};
-			node.X = e.clientX;
-			node.Y = e.clientY;
-			return node;
-		}
-		var currNode = createNode();
-		nodes.push(currNode);
-		//create edge data
-		function createEdge(node){
-			var edge = {};
-			edge.first = [currNode.X, currNode.Y];
-			edge.second = [node.X, node.Y];
-			return edge;
-		}
-		//go through each node and check if edge is possible without overlap
-		nodes.forEach(function(node){
-			if(node.X != currNode.X && node.Y != currNode.Y){
-				var noOverlap = true
-				var currEdge = createEdge(node);
-				for(var i=0;i<edges.length;i++){
-					if(checkOverlap(currEdge, edges[i])){
-						noOverlap = false;
-						break;
-					}
+		$('body').on('mouseup', function(upEvent){
+			setTimeout(function(){
+				isDrag = false;
+			});
+			$('body').off('mousemove mouseup');
+			var min = Infinity;
+			var nearestNode;
+			nodes.forEach(function(node){
+				var dist = getDistance(node, {X : upEvent.clientX, Y : upEvent.clientY});
+				if(dist < min){
+					min = dist
+					nearestNode = node; 
 				}
-				if(noOverlap){
-					edges.push(currEdge);
-					var ctx = $('#grid')[0].getContext("2d");
-					ctx.strokeStyle = 'black';
-					ctx.beginPath();
-					ctx.moveTo(currEdge.first[0], currEdge.first[1]);
-					ctx.lineTo(currEdge.second[0], currEdge.second[1]);
-					ctx.stroke();
-				}		
+			});
+			el.css({
+				top: nearestNode.Y - 10,
+				left: nearestNode.X - 10,
+				transition: '0.5s all'
+			});
+			setTimeout(function(){
+				el.css({
+					transition: 'none'
+				});
+			}, 500);
+			//set starting and ending nodes
+			if(el.hasClass('start')){
+				startNode = nearestNode;
+			}
+			if(el.hasClass('end')){
+				endNode = nearestNode;
 			}
 		});
 	});
 }
+//distance function
+function getDistance(node1, node2){
+	return Math.sqrt(Math.pow(node2.Y - node1.Y, 2) + Math.pow(node2.X - node1.X, 2));
+}
 
 //find max x-coordinate of edge
 function max(edge){
-	if(edge.first[0] > edge.second[0]){
-		return edge.first[0];
+	if(edge.first.X > edge.second.X){
+		return edge.first.X;
 	}else{
-		return edge.second[0];
+		return edge.second.X;
 	}
 }
 //find min x-coordinate of edge
 function min(edge){
-	if(edge.first[0] > edge.second[0]){
-		return edge.second[0];
+	if(edge.first.X < edge.second.X){
+		return edge.first.X;
 	}else{
-		return edge.first[0];
-	}}
+		return edge.second.X;
+	}
+}
 
 
 //check if edges overlaps
 function checkOverlap(edge1, edge2){
-	var a1x = edge1.first[0];
-	var a1y = edge1.first[1];
-	var a2x = edge1.second[0];
-	var a2y = edge1.second[1];
+	var a1x = edge1.first.X;
+	var a1y = edge1.first.Y;
+	var a2x = edge1.second.X;
+	var a2y = edge1.second.Y;
 	
-	var b1x = edge2.first[0];
-	var b1y = edge2.first[1];
-	var b2x = edge2.second[0];
-	var b2y = edge2.second[1];
+	var b1x = edge2.first.X;
+	var b1y = edge2.first.Y;
+	var b2x = edge2.second.X;
+	var b2y = edge2.second.Y;
 	var bounds1, bounds2 = false;
 	
 	//evaluate special cases
@@ -108,3 +111,89 @@ function checkOverlap(edge1, edge2){
 		return false;
 	}
 }
+
+function main(e){
+	$('#grid').attr("height", $(document).height());
+	$('#grid').attr("width", $(document).width());
+	
+	$('body').click(function(e){
+	
+		//check drag
+		if(isDrag) return false;
+		//display node data
+		var nodeContainer = $("<div>").addClass('node');
+		$("body").append(nodeContainer);
+		nodeContainer.css({
+			top: e.clientY - nodeContainer.height()/2,
+			left: e.clientX - nodeContainer.width()/2
+		});
+		//create node data
+		function createNode(){
+			var node = {};
+			node.X = e.clientX;
+			node.Y = e.clientY;
+			node.adj = [];
+			node.visited = false;
+			return node;
+		}
+		var currNode = createNode();
+		nodes.push(currNode);
+		//create start and end nodes
+		if(nodes.length == 1){
+			var start = $("<div>").addClass('start');
+			makeDraggable(start);
+			$('body').append(start);
+			start.css({
+				top: currNode.Y - start.height()/2,
+				left: currNode.X - start.width()/2,
+				border: '2px solid green'
+			});
+			startNode = currNode;
+		}else if(nodes.length == 2){
+			var end = $("<div>").addClass('end');
+			makeDraggable(end);
+			$('body').append(end);
+			end.css({
+				top: currNode.Y - end.height()/2,
+				left: currNode.X - end.width()/2,
+				border: '2px solid red'
+			});
+			endNode = currNode;
+		}
+		//create edge data
+		function createEdge(node){
+			var edge = {};
+			edge.first = currNode;
+			edge.second = node;
+			edge.dist = getDistance(currNode, node);
+			return edge;
+		}
+		//go through each node and check if edge is possible without overlap
+		nodes.forEach(function(node){
+			if(node.X != currNode.X && node.Y != currNode.Y){
+				var noOverlap = true
+				var currEdge = createEdge(node);
+				for(var i=0;i<edges.length;i++){
+					if(checkOverlap(currEdge, edges[i])){
+						noOverlap = false;
+						break;
+					}
+				}
+				if(noOverlap){
+					var ctx = $('#grid')[0].getContext("2d");
+					ctx.strokeStyle = 'black';
+					ctx.beginPath();
+					ctx.moveTo(currEdge.first.X, currEdge.first.Y);
+					ctx.lineTo(currEdge.second.X, currEdge.second.Y);
+					ctx.stroke();
+					currEdge.brush = ctx;
+					edges.push(currEdge);
+					currNode.adj.push(currEdge);
+
+				}		
+			}
+		});
+	});
+}
+
+$(window).ready(main);
